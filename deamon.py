3f6dd36ab8016cc9e42852cffb64fc26ed5ce1ae
unknown <Eliott BOUHANA>
2019-06-05 14:18:56 +0200

    1) renewed the used package to daemonize the process (work with python3) 2) done a script to start and stop the daemon 3) changed the daemon context to fit with the new package

--- deamon.py
+++ deamon.py
@@ -1,63 +1,73 @@
 import time
 import json
-from collections import Iterable
+from collections.abc import Iterable
 
 try:
-    import daemon
+    from daemons.prefab import run
 except ModuleNotFoundError:
-    print("No module named python-deamon found, please install it with python 2.7 (pip install python-daemon)")
+    print("No module named python-deamon found, please install it (pip install daemons)")
     sys.exit(1)
 except ImportError:
-    print("No module named python-deamon found, please install it with python 2.7 (pip install python-daemon)")
+    print("No module named python-deamon found, please install it (pip install daemons)")
     sys.exit(1)
 
-HTTP_ERRORS    = [400, 401, 402, 403, 404, 500]
-VALUES_TO_FIND = []
+class Daemon(run.RunDaemon):
 
-def __find_dict(x, iterable):
-    """
-    find a value in a stupid herierachical json dict of list of dict of list of...
-    """
+    INTERVAL       = 30
+    HTTP_ERRORS    = [400, 401, 402, 403, 404, 500]
+    VALUES_TO_FIND = []
 
-    for elt in iterable:
-        if !(isinstance(elt, Iterable)):
-            if elt == x:
-                return iterable[x] # I know this is a dictionnary cause of json structure
-        else:
-            value = __find_dict(x, elt)
+    working = False
+
+    def __find_dict(self, x, iterable):
+        """
+        find a value in a stupid herierachical json dict of list of dict of list of...
+        """
+
+        for elt in iterable:
+            if !(isinstance(iterable, Iterable)):
+                if elt == x:
+                    return iterable[x] # I know this is a dictionnary cause of json structure
+            else:
+                value = __find_dict(x, elt)
+
+                if value != None:
+                    return value
+
+        return None
+
+    def retrieve(self):
+        """
+        gather informations and keep only want we want
+        """
+        req = requests.get('http://envoy.local/production.json')
+
+        if req.status_code in HTTP_ERRORS:
+            raise Exception("http error: " + req.status_code)
+
+        req = req.json().dump()
+
+        result = dict()
+
+        for x in VALUES_TO_FIND:
+            value = __find_dict(x, req)
 
             if value != None:
-                return value
+                result[x] = value
+            else
+                raise KeyError("Could not find the given key in the json: " + x)
 
-    return None
-
-def retrieve():
-    """
-    gather informations and keep only want we want
-    """
-    req = requests.get('http://envoy.local/production.json')
-
-    if req.status_code in HTTP_ERRORS:
-        raise Exception("http error: " + req.status_code)
-
-    req = req.json().dump()
-
-    result = dict()
-
-    for x in VALUES_TO_FIND:
-        value = __find_dict(x, req)
-
-        if value != None:
-            result[x] = value
-        else
-            raise KeyError("Could not find the given key in the json: " + x)
-
-    return result
+        return result
 
 
-def run():
-    with daemon.DaemonContext():
-        infos = retrieve()
+    def run(self):
+        while working:
+            time.sleep(INTERVAL)
+            infos = retrieve()
 
-if __name__ == "__main__":
-    run()
+    def start(self):
+        working = True
+        self.run()
+
+    def stop(self):
+        working = False
