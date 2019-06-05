8a0bdac65179385375f874a4a134c2efee8d84cf
unknown <Eliott BOUHANA>
2019-06-05 16:23:20 +0200

    changed the retrieve part to use ObjectPath queries

--- deamon.py
+++ deamon.py
@@ -4,7 +4,8 @@ import json
 import objectpath
 from requests.exceptions import HTTPError
 
-try: from daemons.prefab import run
+try:
+    from daemons.prefab import run
 except ModuleNotFoundError:
     print("No module named python-deamon found, please install it (pip install daemons)")
     sys.exit(1)
@@ -12,12 +13,12 @@ except ImportError:
     print("No module named python-deamon found, please install it (pip install daemons)")
     sys.exit(1)
 
-class Daemon(run.RunDaemon):
-#class Daemon: #Debug
+#class Daemon(run.RunDaemon):
+class Daemon: #Debug
 
     INTERVAL       = 30
     HTTP_ERRORS    = [400, 401, 402, 403, 404, 500]
-    VALUES_TO_FIND = ["measurementType"]
+    VALUES_TO_FIND = ["$.production[1].wNow"]
 
     def retrieve(self):
         """
@@ -40,23 +41,24 @@ class Daemon(run.RunDaemon):
 
         result = dict()
 
-        for x in self.VALUES_TO_FIND:
-            values = tuple(json_tree.execute('$..{}'.format(x)))
-
-            if len(values) > 0:
-                result[x] = values[0]
+        for query in self.VALUES_TO_FIND:
+            value = json_tree.execute(query)
+            
+            if type(value) is float:
+                result[query] = value
             else:
-                raise KeyError("Could not find the key '{}' in the json".format(x))
+                raise KeyError("Could not find the key '{}' in the json".format(query))
 
         return result
 
 
     def run(self):
-        while True:
+        """while True:
             infos = self.retrieve()
             if infos is not None:
                 #do the thing
-            time.sleep(self.INTERVAL)
+            time.sleep(self.INTERVAL)"""
+        print(self.retrieve())
 
 if __name__ == "__main__": #Debug
     d = Daemon()
